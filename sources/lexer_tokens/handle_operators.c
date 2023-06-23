17adb5592fe9e396cb42a0a07744a3e39bff75c4
Carlos Menke <cmenke@c2s19d123.42wolfsburg.de>
2023-06-23 15:57:28 +0200

    addes a function to find cases where $ need to be replaced with value

--- sources/lexer_tokens/handle_operators.c
+++ sources/lexer_tokens/handle_operators.c
@@ -6,7 +6,7 @@
 /*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 19:50:59 by cmenke            #+#    #+#             */
-/*   Updated: 2023/06/23 15:08:03 by cmenke           ###   ########.fr       */
+/*   Updated: 2023/06/23 15:57:01 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,49 +108,84 @@ char	*ft_trim_variable_value(char *string)
 	return (result);
 }
 
-bool	ft_expand_variable(t_tokens *token)
+char	*ft_replace_variale_with_value(char *string)
 {
-	//search for single quotes - skip block
-	//seachr for double quotes - expand variable preserve spaces
-	//search for $? - expand variable in place - ignore the rest of the text after ?
-	//search for $ - expand variable in place - trim spaces
-	char	*string;
 	int		i;
-	bool	in_double_quote;
-	bool	valid;
+	bool	double_quote;
 
-	in_double_quote = false;
-	valid = true;
-	string = token->token;
 	i = 0;
-	// while (string[i])
-	// {
-	// 	if (string[i] == '\'')
-	// 		ft_skip_single_quotes(string, &i);
-	// 	if (string[i] == '\"' && in_double_quote == false)
-	// 		in_double_quote = true;
-	// 	else
-	// 		in_double_quote = false;
-	// 	if (string[i] == '$' && in_double_quote == false)
-	// 		valid = ft_expand_variable_in_place(string, &i);
-	// 	else if (string[i] == '$' && in_double_quote == true)
-	// 		valid = ft_expand_variable_preserve_spaces(string, &i);
-	// 	else
-	// 		i++;
-	// }
+	double_quote = false;
+	printf(BOLD_YELLOW "input string: %s\n\n" STYLE_DEF, string);
 	while (string[i])
 	{
-		if (string[i] == '$' && (string[i + 1] == '\'' || string[i + 1] == '\"'))
-			ft_remoove_dollar_sign(string, &i);
-			valid = ft_expand_variable_in_place(string, &i);
-		else if (string[i] == '$')
-			valid = ft_expand_variable_preserve_spaces(string, &i);
-		else
-			i++;
+		if (string[i] == '\"' && double_quote == false)
+			double_quote = true;
+		else if (string[i] == '\"' && double_quote == true)
+			double_quote = false;
+		if (string[i] == '\'' && double_quote == false)
+			ft_skip_single_quotes(string, &i);
+		if (string[i] == '$' && string[i + 1] == '?')
+			printf("found $? - replace with last exit code\n");
+		else if (string[i] == '$' && string[i] == '\"')
+			printf("found $\" -- just remove the '$'\n");
+		else if (string[i] == '$' && !(string[i + 1] == ' '
+		|| string[i + 1] == '\t' || string[i + 1] == '\n') && double_quote == false)
+		{
+			//find the name of the variable
+			printf("found $ - replace with variable value - trim spaces\n");
+		}
+		else if (string[i] == '$' && double_quote == true)
+		{
+			//find the name of the variable
+			printf("found $ - replace with variable value, preserve spaces\n");
+		}
 		i++;
 	}
-	return (valid);
 }
+
+// bool	ft_expand_variable(t_tokens *token)
+// {
+// 	//search for single quotes - skip block
+// 	//seachr for double quotes - expand variable preserve spaces
+// 	//search for $? - expand variable in place - ignore the rest of the text after ?
+// 	//search for $ - expand variable in place - trim spaces
+// 	char	*string;
+// 	int		i;
+// 	bool	in_double_quote;
+// 	bool	valid;
+
+// 	in_double_quote = false;
+// 	valid = true;
+// 	string = token->token;
+// 	i = 0;
+// 	// while (string[i])
+// 	// {
+// 	// 	if (string[i] == '\'')
+// 	// 		ft_skip_single_quotes(string, &i);
+// 	// 	if (string[i] == '\"' && in_double_quote == false)
+// 	// 		in_double_quote = true;
+// 	// 	else
+// 	// 		in_double_quote = false;
+// 	// 	if (string[i] == '$' && in_double_quote == false)
+// 	// 		valid = ft_expand_variable_in_place(string, &i);
+// 	// 	else if (string[i] == '$' && in_double_quote == true)
+// 	// 		valid = ft_expand_variable_preserve_spaces(string, &i);
+// 	// 	else
+// 	// 		i++;
+// 	// }
+// 	while (string[i])
+// 	{
+// 		if (string[i] == '$' && (string[i + 1] == '\'' || string[i + 1] == '\"'))
+// 			ft_remoove_dollar_sign(string, &i);
+// 			valid = ft_expand_variable_in_place(string, &i);
+// 		else if (string[i] == '$')
+// 			valid = ft_expand_variable_preserve_spaces(string, &i);
+// 		else
+// 			i++;
+// 		i++;
+// 	}
+// 	return (valid);
+// }
 
 bool	ft_variable_expansion(t_data *data)
 {
@@ -167,4 +202,16 @@ bool	ft_handle_operators(t_data *data)
 	//2. here_doc
 	//3. redirection
 
+}
+
+
+int main(void)
+{
+	char *test_string = "echo $HOME $USER $PATH $PWD $OLDPWD $?";
+	char *test_string2 = "echo $HOME $USER \"$PATH $PWD $OLDPWD\"$? $"" $ ";
+	char *result;
+
+	result = ft_replace_variale_with_value(test_string);
+	printf("\n");
+	result = ft_replace_variale_with_value(test_string2);
 }
