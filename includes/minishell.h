72c32b0e0ef90002e67f4cc86f53e94a9b2bf329
Carlos Menke <cmenke@c2s14d74.42wolfsburg.de>
2023-07-10 17:36:59 +0200

    clearing for fresh approach on parsing

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 00:10:38 by cmenke            #+#    #+#             */
-/*   Updated: 2023/06/24 19:55:12 by user             ###   ########.fr       */
+/*   Updated: 2023/07/10 17:29:59 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,106 +45,51 @@
 # define SQUOTE_ERROR "minishell: syntax error while looking for matching `''"
 # define DQUOTE_ERROR "minishell: syntax error while looking for matching `\"'"
 
-
-/// @brief The main data structure for a command.
-/// @details This strucutre stores the information for one command.
-/// @param envp_paths The paths to the commands. PATH= from envp.
-/// @param cmd_path The path to the command.
-/// @param cmd_args The arguments for the command.
-/// @param input_fd The file descriptor for the input.
-/// @param output_fd The file descriptor for the output.
-/// @param next The next command in the command sequence.
-typedef struct s_cmd_in_child
+typedef struct s_shell_data
 {
-	//start at 0
-	int						cmd_index;
-	char					**envp_paths;
-	char					*cmd_path;
-	char					**cmd_args;
-	//initially set to -1
-	int						input_fd;
-	//initially set to -1
-	int						output_fd;
-	struct s_cmd_in_child	*next;
-}							t_child_cmd;
+	char	*command_line_read;
+	bool	add_line_to_history;
+	char	**envp;
+	t_list	*command_sequences;
+}				t_shell_data;
 
-/// @brief The data structure for the tokens of a command line.
-/// @details The node stores the information for one token.
-/// @param token_index The index of the token in the command line.
-/// @param token_type The type of the token.
-/// @param token The token itself.
-/// @param next The next token in the command line.
+typedef struct s_command_sequences
+{
+	char	*sequence;
+	t_list	*tokens;
+	char	*command_path;
+	char	**args;
+	int		input_fd;
+	int		output_fd;
+}				t_command_sequences;
+
 typedef struct s_tokens
 {
-	int					token_index;
-	int					token_type;
-	char				*token;
-	struct s_tokens		*next;
-}						t_tokens;
+	char	*token;
+	char	type;
+}				t_tokens;
 
+// enum e_token_type
+// {
+// 	/* data */
+// };
 
-/// @brief The main data structure for minishell.
-/// @param command The struct for one command of the command sequence.
-/// @param envp The environment variables.
-/// @param pipes All pipes that are used for the complete command sequence.
-/// @param nbr_cmds The number of commands in the command sequence.
-typedef struct s_data
-{
-	char			*line_read;
-	bool			finished_input;
-	t_child_cmd		*command;
-	t_tokens		*tokens;
-	int				token_index;
-	char			**envp;
-	int				**pipes;
-	int				*pids;
-	int				nbr_cmds;
-}					t_data;
+//main
+	//main.c
+// bool	ft_process_command_line(t_shell_data *shell_data);
 
-
-enum	e_token_id
-{
-	enum_word,
-	command,
-	single_quote,
-	double_quote,
-	op_input_redirection,
-	op_here_doc,
-	op_output_redirection_trunc,
-	op_output_redirection_append,
-	op_pipe,
-	token_error
-};
-
-//sources
-//fork_childs.c
-void	ft_child_process_executor(t_data *data, t_child_cmd *command ,int i);
-int		ft_fork_childs(t_data *data, int nbr_cmds);
-//builtins
-// echo.c
-void	ft_echo(char **command);
+//lexer
+	//token_creation.c
+bool	ft_extract_command_sequences(t_shell_data *shell_data);
+bool	ft_create_command_sequence(char *command_line_read, t_list **command_sequences);
+	// syntax_error.c
+bool	ft_check_equal_quote_amt(char *s);
 // clearing
-// ft_free_double_pointer.c
+	//ft_free_double_pointer.c
 void	ft_free_double_pointer_char(char ***ptr);
 void	ft_free_double_pointer_int(int ***ptr);
-//clear_all.c
-void	ft_clear_all_data(t_data *data);
-//execution
-// get_envp_cmd_paths.c
-char	**ft_get_envp_paths(char **envp);
-char	*ft_get_cmd_path(char **envp_paths, char *cmd);
-// lexer_tokens
-// lexer.c
-bool	ft_create_tokens(t_data *data, char *line_read);
-void	ft_skip_to_next_non_delim(char *line_read, int *i, int *start);
-void	ft_skip_quote_block(char *cmd_line, int *i);
-
-// token_analysis.c
-bool	ft_put_id_to_token(t_data *data);
-bool	ft_syntax_in_tokens(t_data *data);
-// handle_operators.c
-bool	ft_handle_operators(t_data *data);
-// remove_quotes.c
-bool	ft_remove_quotes_from_elements(t_tokens *token);
+// void	ft_free_command_sequences(void *command_sequences);
+//helpers.c
+	// void ft_print_list(t_list *list);
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wmoughar <wmoughar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 00:30:06 by cmenke            #+#    #+#             */
-/*   Updated: 2023/07/10 16:44:47 by wmoughar         ###   ########.fr       */
+/*   Updated: 2023/07/10 17:36:37 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,81 +46,15 @@ void ft_sig_sigint_handler(int sig_num)
 }
 
 
-
-bool	ft_initialize_command_struct(t_data *data, char **envp)
-{
-	t_child_cmd *commands;
-
-	commands = ft_calloc(1, sizeof(t_child_cmd));
-	if (!commands)
-	{
-		perror("command struct allocation error");
-		return (false);
-	}
-	commands->cmd_path = "/bin/cat";
-	commands->cmd_args = ft_calloc(3, sizeof(char *));
-	// commands->cmd_args[0] = "echo";
-	// commands->cmd_args[0] = "./minishell";
-	// commands->cmd_args[0] = "../../../../../../bin/ls";
-	// commands->cmd_args[0] = "/bin/ls";
-	commands->cmd_args[0] = "cat";
-	// commands->cmd_args[1] = "test";
-	commands->cmd_args[1] = NULL;
-	commands->input_fd = STDIN_FILENO;
-	mode_t	mode;
-	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-	commands->output_fd = open("output", O_WRONLY | O_TRUNC | O_CREAT, mode);
-	commands->next = NULL;
-	data->command = commands;
-	data->envp = envp;
-	return (true);
-}
-
-void ft_print_tokens(t_data *data, bool last_print)
-{
-	t_tokens *temp;
-	t_tokens *temp2;
-
-	temp2 = data->tokens;
-	while (temp2)
-	{
-		temp = temp2;
-		printf(BOLD_BLUE"token:"STYLE_DEF"%s\n", temp2->token);
-		temp2 = temp2->next;
-		if (last_print == true)
-		{
-			free(temp->token);
-			free(temp);
-		}
-	}
-	if (last_print == true)
-		data->tokens = NULL;
-}
-
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line_read;
 	t_data	*data;
 
-	if (argc > 1)
-	{
-		// if (!ft_strncmp(argv[1], "env", ft_strlen(argv[1])))
-		// 	print_env(envp);
-		// if ((!ft_strncmp(argv[1], "export", ft_strlen(argv[1]))) && !argv[2])
-		// {
-		// 	export(envp);
-		// }
-	}
 	// ft_print_double_pointer(envp);
 	data = ft_calloc(1, sizeof(t_data));
 	if (!data)
 		perror("struct allocation error");
-	// ft_initialize_command_struct(data, envp);
-	// printf(BOLD_PINK"Minishell START\n"STYLE_DEFAULT);
-	// printf("exit_code last child:%d\n", ft_fork_childs(data, 2));
-	// printf(BOLD_PINK"Minishell END\n"STYLE_DEFAULT);
-	// exit(0);
-
 	//to ignore the SIG_QUIT signal from ctrl- Backslash
 	signal(SIGQUIT, SIG_IGN);
 	// handles the ctrl-C key.
@@ -146,24 +80,6 @@ int	main(int argc, char **argv, char **envp)
 			// 	add_to_list(env, line_read);
 			// else
 			// 	remove_from_list(env, line_read);
-			//work with the line read
-			// while (data->finished_input == false)
-			// {
-			// 	if (ft_create_tokens(data, line_read) == false)
-			// 		ft_putendl_fd("token creation error", STDERR_FILENO);
-			// 	// ft_print_tokens(data, false);
-			// 	printf(BOLD_PINK "check for syntax errros\n\n" STYLE_DEF);
-			// 	if (ft_syntax_in_tokens(data) == false)
-			// 		data->finished_input = true;
-			// 	printf("%s\n", data->tokens->token);
-			// 	ft_print_tokens(data, false);
-			// 	printf(BOLD_PINK "Finished syntax check\n\n" STYLE_DEF);
-			// 	printf(BOLD_PINK "Handling operators\n\n" STYLE_DEF);
-			// 	if (ft_handle_operators(data) == false)
-			// 		data->finished_input = true;
-			// 	printf(BOLD_PINK "Finished handling operators\n\n" STYLE_DEF);
-			// 	ft_print_tokens(data, true);
-			// }
 			free(line_read);
 		}
 		else if (!line_read)
