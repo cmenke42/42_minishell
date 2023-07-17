1dd6e29fe83aa65e52385182eda7f963861afa26
Carlos Menke <cmenke@c2s19d124.42wolfsburg.de>
2023-07-17 15:53:07 +0200

    PID seems to work as well

--- sources/lexer/expand_variables.c
+++ sources/lexer/expand_variables.c
@@ -6,7 +6,7 @@
 /*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 18:32:40 by cmenke            #+#    #+#             */
-/*   Updated: 2023/07/17 15:05:35 by cmenke           ###   ########.fr       */
+/*   Updated: 2023/07/17 15:52:33 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ bool	ft_expand_variable(t_list *tokens, t_env *env_list)
 {
 	while (tokens)
 	{
+		//free the variable name
 		if (!ft_do_variable_expansion((t_tokens *)tokens->content, env_list))
 		{
 			printf("some error with the expansion\n");
@@ -46,14 +47,26 @@ bool	ft_expand_variable(t_list *tokens, t_env *env_list)
 bool	ft_do_variable_expansion(t_tokens *token, t_env *env_list)
 {
 	char	*string;
+	bool	in_double_quotes;
+	bool	in_single_quotes;
 
 	string = token->token;
+	in_double_quotes = false;
+	in_single_quotes = false;
 	//determine wheter the $ is in single, double or no quotes
 	while (*string)
 	{
-		if (*string == '$')
+		if (!in_single_quotes && *string == '\'')
+			in_single_quotes = true;
+		else if (in_single_quotes && *string == '\'')
+			in_single_quotes = false;
+		if (!in_double_quotes && !in_single_quotes && *string == '\"')
+			in_double_quotes = true;
+		else if (in_double_quotes && !in_single_quotes && *string == '\"')
+			in_double_quotes = false;
+		if (*string == '$' && !in_single_quotes)
 		{
-			if (!ft_execute_specific_case_of_variable_expansion(&string, &token->token, false, env_list))
+			if (!ft_execute_specific_case_of_variable_expansion(&string, &token->token, in_double_quotes, env_list))
 				return (false); //maybe clear something
 		}
 		else
@@ -65,9 +78,11 @@ bool	ft_do_variable_expansion(t_tokens *token, t_env *env_list)
 //string starts at the $
 bool	ft_execute_specific_case_of_variable_expansion(char	**string, char **token, bool in_double_quotes, t_env *env_list)
 {
-	char *variable_name;
+	char	*variable_name;
+	char	*process_id;
 
 	variable_name = NULL;
+	process_id = NULL;
 	//keep the dollar sign
 	// $ \0     $\t  \0 || '$' || "$''" "$""" || $\0
 	if (ft_is_whitespace(*(*string + 1))
@@ -80,15 +95,30 @@ bool	ft_execute_specific_case_of_variable_expansion(char	**string, char **token,
 	//remove the dollar sign
 	// $"abc"  $'abc'
 	else if (!in_double_quotes && ft_is_char_quote(*(*string + 1)))
+	{
 		printf("remove the goddam $\n");
+		if (!ft_replace_variable_name_with_value(string, token, NULL, NULL))
+			return (false);
+	}
 	//PID speacial variable $$
 	else if ((*(*string + 1)) == '$')
-		printf("replace with PID of the current shell or script."); //or what else to with it
+	{
+		printf("replace with PID of the current shell or script.\n"); //or what else to with it
+		// free the process_id
+		process_id = ft_itoa((int)getpid());
+		if (!process_id)
+			return (false);
+		printf("PID:%s", process_id);
+		if (!ft_replace_variable_name_with_value(string, token, NULL, process_id))
+			return (false);
+	}
 	//replace with value
 	else if (in_double_quotes)
 	{
 		printf("replace with value\n");
 		if (!ft_get_variable_name(*string, &variable_name))
+			return (false);
+		if (!ft_replace_variable_name_with_value(string, token, variable_name, ft_get_variable_value(env_list, variable_name)))
 			return (false);
 	}
 	//replace with trimmed value
@@ -133,6 +163,8 @@ char	*ft_get_variable_value(t_env *env, char	*variable_name)
 	t_env *tmp;
     char *value;
 
+	if(!variable_name)
+		return (NULL);
 	value = NULL;
     tmp = ft_search(env, variable_name);
     if (!tmp)
@@ -150,41 +182,55 @@ bool	ft_replace_variable_name_with_value(char **string, char **token, char *name
 	int		name_len;
 
 
-	// printf("len:")
+	//for the $$ PID
+	name_len = 1;
+	if((*(*string + 1)) == '$')
+		name_len = 2;
+	first_part = NULL;
 	if (name)
-		name_len = ft_strlen(name);
-	first_part = ft_substr(*token, 0, *string - *token);
-	if (!first_part)
-		return (false);
-	// printf("first_part:%s\n", first_part);
-	if (value)
+		name_len += ft_strlen(name);
+	if (*string - *token > 0)
+	{
+		first_part = ft_substr(*token, 0, *string - *token);
+		if (!first_part)
+			return (false);
+		printf("first_part:%s\n", first_part);
+	}
+	if (first_part)
 	{
 		first_part_and_value = ft_strjoin(first_part, value);
-		free(first_part);
 		if (!first_part_and_value)
 			return (false);
-		// printf("first_part_and_value:%s\n", first_part_and_value);
+		printf("first_part_and_value:%s\n", first_part_and_value);
 	}
-	//+1 for the $;
-	last_part = ft_strdup(*string + name_len + 1);
+	printf("hello\n");
+	// +1 for the $;
+	last_part = ft_strdup(*string + name_len);
 	if (!last_part)
 	{
 		if (first_part_and_value)
 			free(first_part_and_value);
-		free(first_part);
+		if (first_part)
+			free(first_part);
 	}
-	// printf("last_part:%s\n", last_part);
+	printf("last_part:%s\n", last_part);
 	if (value)
 	{
 		result = ft_strjoin(first_part_and_value, last_part);
 		free(first_part_and_value);
+		free(last_part);
+	}
+	else if (!first_part)
+	{
+		result = last_part;
 	}
 	else
 	{
 		result = ft_strjoin(first_part, last_part);
-		free(first_part);
+		if (first_part)
+			free(first_part);
+		free(last_part);
 	}
-	free(last_part);
 	if (!result)
 		return (false);
 	printf("result:%s\n", result);
@@ -194,3 +240,4 @@ bool	ft_replace_variable_name_with_value(char **string, char **token, char *name
 	return (true);
 }
 
+// $'HOME'
